20a837da20b0fa7aa4a2e9e0abc679ccd5105ec6
Dat Hoang <fiezt@outlook.com>
2024-04-14 15:04:17 +0700

    deleted leftovers

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
-router.use('/books', require('./books'))
 router.use('/users', require('./users'))
 router.use('/auth', require('./auth'))
-router.use('/authors', require('./authors'))
 router.use('/rooms', require('./rooms'))
-router.use('/messages', require('./rooms'))
 
 module.exports = router
